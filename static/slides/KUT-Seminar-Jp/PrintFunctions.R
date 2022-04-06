0036b4fd5205beb2092586a795888ddc96cd2919
Kostya <k.ovsiannikov@gmail.com>
2022-04-06 10:13:03 +0900

    correct typo in jp presentation

--- static/slides/KUT-Seminar-Jp/PrintFunctions.R
+++ static/slides/KUT-Seminar-Jp/PrintFunctions.R
@@ -6,3 +6,5 @@ library(here)
 ------------------
 
 build_pdf(here("static", "slides", "KUT-Seminar-Jp", "KUT-Seminar-Jp.Rmd"))
+
+#build_pdf("https://kovsiannikov.com/slides/kut-seminar-jp/kut-seminar-jp")
